5c73e0f37f928a736da970358d60d9618e298dcf
liliangwei <llwei89@gwmail.gwu.edu>
2014-09-17 21:23:11 -0400

    add one different governing equation

--- matlab/fem1d.m
+++ matlab/fem1d.m
@@ -9,7 +9,7 @@ format short e
 
 x0 = 0; x1 = 1;   % start and end point
 L = x1 - x0;      % total length
-N = 5;            % number of interval
+N = 8;            % number of interval
 dx = L/N;         % interval size
 
 % test case 1 condition
@@ -19,17 +19,19 @@ u = zeros(1,N+1); A = zeros(N+1,N+1); F = zeros(1,N+1);
 isBdary = false(N+1,1); x = linspace(0,1,N+1);
 % Element stiffness matrix and load vector
 Ak = 1/dx*[1,-1;-1,1];
-Fk = dx/2*[1;1];
 % Assemble global matrix
 for i=1:N
     for m=1:2
         for n=1:2
    A(m+i-1,n+i-1) = A(m+i-1,n+i-1) + Ak(m,n);
         end
+        a = x(i); b = x(i+1);
+        Fk = [1/dx*(sin(2*pi*b)-sin(2*pi*a))-2*pi*cos(2*pi*a);
+              1/dx*(sin(2*pi*a)-sin(2*pi*b))+2*pi*cos(2*pi*b)];
         F(m+i-1) = F(m+i-1) + Fk(m);
     end
 end
-F = -2*F';
+F = F';
 
 % Apply Boundary condition
 isBdary(1) = true;isBdary(N+1) = true;
@@ -37,7 +39,7 @@ free = find(~isBdary);
 
 % Calculate results
 u(free) = A(free,free)\F(free);
-u_exact = -x.*(1-x);
+u_exact = -sin(2*pi*x);
 
 % Error analysis
 error = sqrt(sum((u_exact-u).^2))/N;
