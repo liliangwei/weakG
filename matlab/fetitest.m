9b3b6654a95847aca3e592288faef8e90903596d
liliangwei <llwei89@gwmail.gwu.edu>
2014-09-23 18:54:44 -0400

    poission FETI test

--- matlab/fetitest.m
+++ matlab/fetitest.m
@@ -10,11 +10,24 @@ L = 1;
 N = 9;
 dx = L/N;
 x = linspace(0,1,N+1);
-f1 = zeros(1,3); f3 = zeros(1,3); f2 = zeros(1,4);
-for i=1:3
-    
-%Fk = [1/dx*(sin(2*pi*b)-sin(2*pi*a))-2*pi*cos(2*pi*a);
- %     1/dx*(sin(2*pi*a)-sin(2*pi*b))+2*pi*cos(2*pi*b)];
+f1 = zeros(1,4); f3 = zeros(1,4); f2 = zeros(1,4);
+for i=1:N
+    a = x(i); b = x(i+1);   
+    Fk = [1/dx*(sin(2*pi*b)-sin(2*pi*a))-2*pi*cos(2*pi*a);
+      1/dx*(sin(2*pi*a)-sin(2*pi*b))+2*pi*cos(2*pi*b)];
+  if(i<4)
+      for m=1:2
+          f1(m+i-1) = f1(m+i-1) + Fk(m);
+      end
+  elseif(i<7)
+      for m=1:2
+          f2(m+i-4) = f2(m+i-4) + Fk(m);
+      end
+  else
+      for m=1:2
+          f3(m+i-7) = f3(m+i-7) + Fk(m);
+      end
+  end
 end
 % unknowns of the subdomains
 K1 = 1/dx*[2,-1,0;-1,2,-1;0,-1,1];
@@ -43,3 +56,46 @@ epsilon3 = [0,0,0;1,0,0];
 G = -epsilon1*R1-epsilon2*R2-epsilon3*R3;
 
 % Vector e 
+e = -dot(R2,f2);
+
+% lambda0
+lambda0 = G*inv(dot(G',G))*e;
+
+% F matrix
+F = epsilon1*K1P*epsilon1' + epsilon2*K2P*epsilon2' + epsilon3*K3P*epsilon3';
+
+% Vector g
+g = epsilon1*K1P*f1(2:4)' + epsilon2*K2P*f2' + epsilon3*K3P*f3(1:3)';
+
+% simple one-dimensional example leads to the specially reduced system of
+% equtions where no iteration is necessary
+alpha = inv(dot(G',G))*G'*(g-F*lambda0);
+
+% Displacemenets of the subdomains are obtained from two contributions,
+% dker and dinfi.
+% The dinfi are obtained with the help of pseudo-inverse matrices
+
+dinfi1 = K1P*(f1(2:4)' - epsilon1' * lambda0);
+dinfi2 = K2P*(f2' - epsilon2' * lambda0);
+dinfi3 = K3P*(f3(1:3)' - epsilon3' * lambda0);
+
+% Contribuions from rigid body motions 
+dker1 = R1*0;
+dker2 = R2*alpha;
+dker3 = R3*0;
+
+% The final subdomain displacement vectors are
+d1 = dinfi1 + dker1;
+d2 = dinfi2 + dker2;
+d3 = dinfi3 + dker3;
+
+% plot 
+u1 = zeros(1,4); u2 = zeros(1,4); u3 = zeros(1,4);
+u1(2:4) = d1; u2 = d2; u3(1:3) = d3;
+
+figure
+plot(x(1:4),u1);
+hold on
+plot(x(4:7),u2,'r-');
+hold on
+plot(x(7:10),u3,'g-');
